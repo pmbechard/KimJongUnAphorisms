533daa262cc70b9fe6e9080bf2c3305a3aa90ba4
Peyton Bechard <pbechard@my.yorku.ca>
2022-04-04 19:21:35 +0800

    Refactor for OOP design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 from kju_aphorisms import get_kju_aphorism as aphorism
 from scheduler import *
@@ -9,68 +10,76 @@ import time
 import winsound
 
 
-def alarm_watch():
-    pass
+class App:
+    def __init__(self, root):
+        # Window Configuration
+        self.root = root
+        self.root.title("KJU: Aphorism of the Day")
+        self.root['bg'] = 'lightgray'
+        self.root.resizable(False, False)
+        self.photo = PhotoImage(file='dprk_pub_logo.png')
+        self.root.iconphoto(False, self.photo)
+
+        # Aphorism and Author Text Labels
+        self.aphorism_text = ttk.Label(text=aphorism(), background='lightgray', wraplength=300,
+                                 justify='center')
+        self.aphorism_text.grid(row=0, column=0, columnspan=2, padx=20, pady=20)
+        self.author_text = ttk.Label(text="-Kim Jong Un", background='lightgray')
+        self.author_text.grid(row=2, column=0, sticky='e', columnspan=2, padx=20)
+
+        # Scheduling Options
+        self.schedule_button = ttk.Button(text="Set Schedule")
+        self.schedule_button.grid(row=3, column=1, sticky='e', padx=20, pady=10)
+        self.schedule_button.bind('<ButtonRelease-1>', lambda event: self.new_schedule())
+        self.current_schedule = f"Current Schedule: {get_schedule()}"
+        self.schedule_info = ttk.Label(text=self.current_schedule, background='lightgray')
+        self.schedule_info.grid(row=3, column=0, sticky='w', padx=20, pady=10)
+
+    def alarm_watch(self):
+        pass
+
+    def new_schedule(self):
+        self.schedule_window = Toplevel(self.root)
+        self.schedule_window['bg'] = 'lightgray'
+        self.schedule_window.iconphoto(False, self.photo)
+        self.schedule_window.title("Set Schedule")
+        self.schedule_window.resizable(False, False)
+        self.schedule_window.focus()
+        self.schedule_window.grab_set()
+
+        schedule_label = ttk.Label(self.schedule_window, text='Set a new time for your daily aphorism:',
+                                   background='lightgray')
+        schedule_label.grid(row=0, column=0, columnspan=2, padx=20, pady=10)
+
+        input_box = ttk.Entry(self.schedule_window, width=6)
+        input_box.grid(row=1, column=0, sticky='e', padx=20, pady=10)
+
+        instructions_label = ttk.Label(self.schedule_window, text='e.g. 8:00 or 15:30', background='lightgray')
+        instructions_label.grid(row=1, column=1, sticky='w')
+
+        set_button = ttk.Button(self.schedule_window, text='Set New Time')
+        set_button.grid(row=2, column=0, columnspan=2, pady=10)
+        set_button.bind('<ButtonRelease-1>', lambda event: self.check_input(input_box.get()))
+
+    def check_input(self, user_input):
+        clock_format = r'^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$'
+        user_input = user_input.strip()
+        if user_input and user_input[0] == '0':
+            user_input = user_input[1:]
+        if re.match(clock_format, user_input):
+            self.schedule_info.configure(text=f'Current Schedule: {user_input}')
+            set_schedule(user_input)
+            self.schedule_window.destroy()
+            self.alarm_watch()
+        else:
+            messagebox.showerror(title='Invalid Entry', message='Please enter a valid time.')
 
 
-def new_schedule():
-    schedule_window = tk.Toplevel(main_window)
-    schedule_window['bg'] = 'lightgray'
-    schedule_window.title("Set Schedule")
-    schedule_window.resizable(False, False)
-    schedule_window.focus()
-    schedule_window.grab_set()
-
-    schedule_label = tk.Label(schedule_window, text='Set a new time for your daily aphorism:', background='lightgray', padx=20, pady=10)
-    schedule_label.grid(row=0, column=0, columnspan=2)
-
-    input_box = tk.Entry(schedule_window, width=6)
-    input_box.grid(row=1, column=0, sticky='e', padx=20, pady=10)
-
-    instructions_label = tk.Label(schedule_window, text='e.g. 8:00 or 15:30', background='lightgray')
-    instructions_label.grid(row=1, column=1, sticky='w')
-
-    set_button = tk.Button(schedule_window, text='Set New Time', justify='center')
-    set_button.grid(row=2, column=0, columnspan=2, pady=10)
-    set_button.bind('<ButtonRelease-1>', lambda event: check_input(schedule_window, input_box.get()))
-
-
-def check_input(window, user_input):
-    clock_format = r'^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$'
-    user_input = user_input.strip()
-    if user_input and user_input[0] == '0':
-        user_input = user_input[1:]
-    if re.match(clock_format, user_input):
-        schedule_info.configure(text=f'Current Schedule: {user_input}')
-        set_schedule(user_input)
-        window.destroy()
-    else:
-        tk.messagebox.showerror(title='Invalid Entry', message='Please enter a valid time.')
+def main():
+    root = Tk()
+    App(root)
+    root.mainloop()
 
 
 if __name__ == '__main__':
-    # Window Configuration
-    main_window = tk.Tk()
-    main_window.title("KJU: Aphorism of the Day")
-    main_window['bg'] = 'lightgray'
-    main_window.resizable(False, False)
-    photo = tk.PhotoImage(file='dprk_pub_logo.png')
-    main_window.iconphoto(False, photo)
-
-    # Aphorism and Author Text Labels
-    aphorism_text = tk.Label(text=aphorism(), padx=20, pady=20, background='lightgray', wraplength=300, justify='center')
-    aphorism_text.grid(row=0, column=0, columnspan=2)
-    author_text = tk.Label(text="-Kim Jong Un", background='lightgray', padx=20)
-    author_text.grid(row=2, column=0, sticky='e', columnspan=2)
-
-    # Scheduling Options
-    schedule_button = tk.Button(text="Set Schedule")
-    schedule_button.grid(row=3, column=1, sticky='e', padx=20, pady=10)
-    schedule_button.bind('<ButtonRelease-1>', lambda event: new_schedule())
-    current_schedule = f"Current Schedule: {get_schedule()}"
-    schedule_info = tk.Label(text=current_schedule, background='lightgray', padx=20, pady=10)
-    schedule_info.grid(row=3, column=0, sticky='w')
-
-    # t1 = threading.Thread(task=alarm_watch, name='t1')
-
-    main_window.mainloop()
+    main()
